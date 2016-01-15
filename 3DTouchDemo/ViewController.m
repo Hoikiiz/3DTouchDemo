37a929cc6436001b14a4297ed26d2228a293776b
Hoikiiz <407036069@qq.com>
2016-01-15 14:42:52 +0800

    修改了出事View的位置和形状

--- 3DTouchDemo/ViewController.m
+++ 3DTouchDemo/ViewController.m
@@ -9,12 +9,26 @@
 #import "ViewController.h"
 #import "TouchViewController.h"
 @interface ViewController ()<UIViewControllerPreviewingDelegate>
-@property (weak, nonatomic) IBOutlet UIView *touchView;
+@property (weak, nonatomic) UIView *touchView;
 @property (weak, nonatomic) IBOutlet UILabel *forceLab;
 
 @end
 
 @implementation ViewController
+
+- (UIView *)touchView {
+    if (!_touchView) {
+        UIView *v = [[UIView alloc] init];
+        [self.view addSubview:v];
+        v.backgroundColor = [UIColor colorWithRed:0 green:0.75 blue:0.75 alpha:1];
+        v.frame = CGRectMake(0, 0, 200, 200);
+        v.center = self.view.center;
+        v.layer.cornerRadius = 100;
+        _touchView = v;
+    }
+    return _touchView;
+}
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
